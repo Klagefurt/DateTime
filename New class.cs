a9697a26c90908e126658ad9f1f0e289557bd14c
Nikolay Pomogaev <klagefurt@gmail.com>
2024-02-16 20:13:06 +0300

    Добавил две переменные (подзадача 1)

---
+++ New class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Class1
+{
+	private int val;
+
+	public Class1(int a)
+	{
+		val = a; 
+	}
+}
--- Program.cs
+++ Program.cs
@@ -2,7 +2,10 @@
 {
     private static void Main(string[] args)
     {
+        //новая подзадача
         Console.WriteLine("Hello, World!");
         Console.WriteLine("It is number 1");
+        int b = 45;
+        int c = 32;
     }
 }
